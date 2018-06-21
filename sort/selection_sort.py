cbd4835605b1e94bdb79c3da107f132e085c6ff1
pbdgk <gordonworkspace@gmail.com>
2018-06-21 20:25:18 +0300

    selection sort added

---
+++ sort/selection_sort.py
@@ -0,0 +1,22 @@
+import random
+
+from sort import Sort
+
+
+class SelectionSort(Sort):
+    """Quadratic running time"""
+
+    def _sort(self, items):
+        N = len(items)
+        for i in range(N):
+            min_ = i
+            for j in range(i + 1, N):
+                if self.less(items[j], items[min_]):
+                    min_ = j
+            self.change(items, i, min_)
+
+
+if __name__ == '__main__':
+    my_items = [random.random() for i in range(30)]
+    sorter = SelectionSort()
+    sorter.sort(my_items)
